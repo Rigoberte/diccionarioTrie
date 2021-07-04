d74caa0414de37507bae87ed7eb96324947fe693
Rigoberte <65089651+Rigoberte@users.noreply.github.com>
2021-07-04 20:07:03 -0300

    Update string_map.hpp

--- src/string_map.hpp
+++ src/string_map.hpp
@@ -5,18 +5,26 @@ string_map<T>::string_map(): _size(0), raiz(nullptr){
 
 template <typename T>
 string_map<T>::string_map(const string_map<T>& aCopiar) : string_map() {
-    //*this = aCopiar;
+    *this = aCopiar;
 }
 // Provisto por la catedra: utiliza el operador asignacion para realizar la copia.
 
 template <typename T>
 string_map<T>& string_map<T>::operator=(const string_map<T>& d) {
-    // COMPLETAR
+    for (auto clave: _clavesDefinidas) {
+        erase(clave);
+    }
+
+    for (auto clave: d._clavesDefinidas) {
+        this->insert(make_pair(clave, d.at(clave)));
+    }
 }
 
 template <typename T>
 string_map<T>::~string_map(){
-    // COMPLETAR
+    if (raiz != nullptr){
+        raiz->destruir(raiz);
+    }
 }
 
 template <typename T>
@@ -33,6 +41,7 @@ void string_map<T>::insert(const pair<string, T>& value_type) {
     if (count(clave_.first) == 0){
         _size++;
     }
+    _clavesDefinidas.insert(clave_.first);
 
     for (int i = 0; i < clave_.first.size(); ++i) {
         int valorEnASCII = int(clave_.first[i]);
@@ -56,23 +65,28 @@ void string_map<T>::insert(const pair<string, T>& value_type) {
         }
         nodo_iesimo = nodo_iesimo->siguientes[valorEnASCII];
     }
+
 }
 
 template <typename T>
 int string_map<T>::count(const string& clave) const{
-    Nodo *nodo_iesimo = raiz;
+    Nodo* nodo_iesimo = raiz;
     for (int i = 0; i < clave.size(); ++i) {
         int valorEnASCII = int(clave[i]);
-        if (nodo_iesimo->siguientes[valorEnASCII] == nullptr || (i == clave.size()-1 && nodo_iesimo->definicion == nullptr))
-        {
-            return 0;
-        }
-        else
+        if (nodo_iesimo->siguientes[valorEnASCII] != nullptr)
         {
             nodo_iesimo = nodo_iesimo->siguientes[valorEnASCII];
         }
+        else
+        {
+            return 0;
+        }
     }
-    return 1;
+    if (nodo_iesimo->definicion != nullptr){
+        return 1;
+    }else{
+        return 0;
+    }
 }
 
 template <typename T>
@@ -97,7 +111,14 @@ T& string_map<T>::at(const string& clave) {
 
 template <typename T>
 void string_map<T>::erase(const string& clave) {
-    // COMPLETAR
+    Nodo* nodo_iesimo = raiz;
+    for (int i = 0; i < clave.size(); i++) {
+        int valorEnASCII = int(clave[i]);
+        nodo_iesimo = nodo_iesimo->siguientes[valorEnASCII];
+    }
+    delete nodo_iesimo->definicion;
+    nodo_iesimo->definicion = nullptr;
+    _clavesDefinidas.erase(clave);
 }
 
 template <typename T>
